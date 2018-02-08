81a7710b7defd2512ce4be46177846352514b79f
Serv <mheinrich86@gmail.com>
2018-02-08 22:31:38 +0100

    Initial commit including tests

---
+++ WebExtensions.Tests/ControlCollectionTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Xunit;
+
+namespace WebExtensions.Tests
+{
+	public class ControlCollectionTests
+	{
+		const int ControlCount = 10;
+
+		[Fact]
+		public void PassingTest()
+		{
+			Assert.Equal(4, 2 + 2);
+		}
+
+		internal ControlCollection ArrangeControls()
+		{
+			Control ctrl = new Control();
+			ControlCollection coll = ctrl.Controls;
+
+			for (int i = 0; i < ControlCount; i++)
+			{
+				var button = new Button();
+				button.Text = $"Button {i}";
+
+				var literal = new Label {
+					Text = $"Literal {i}"
+				};
+
+				if(i % 2 == 0)
+				{
+					button.Attributes["data-foo"] = "bar";
+				}
+				coll.Add(button);
+				coll.Add(literal);
+			}
+			return coll;
+		}
+		[Fact]
+		public void AssertCollectionContains10ItemsOfType()
+		{
+			var coll = ArrangeControls();
+			var buttonCount = coll.GetControls<Button>().Count();
+			var labelCount = coll.GetControls<Label>().Count();
+			Assert.Equal(buttonCount, ControlCount);
+			Assert.Equal(labelCount, ControlCount);
+		}
+		
+		[Fact]
+		public void QueryButtonsByAttribute()
+		{
+			var coll = ArrangeControls();
+			var buttonCount = coll.GetControls<Button>( x => x.Attributes["data-foo"] == "bar").Count();
+			Assert.Equal(5, buttonCount);
+
+		}
+
+	}
+}
---
+++ WebExtensions/ControlCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebExtensions
+{
+    public static class ControlCollectionExtensions
+    {
+		public static IEnumerable<T> GetControls<T>(this ControlCollection cCol,
+			List<T> results,
+			Func<T, bool> predicate) where T : WebControl
+		{
+
+			foreach (Control control in cCol)
+			{
+				if ((control is T && predicate == null) || (control is T && predicate != null && predicate((T)control)))
+				{
+					results.Add((T)control);
+				}
+				
+				if (control.HasControls())
+				{
+					GetControls<T>(control.Controls, results, predicate);
+				}
+			}
+
+			return results;
+		}
+
+		public static IEnumerable<T> GetControls<T>(this ControlCollection cCol) where T : WebControl
+		{
+			return cCol.GetControls(new List<T>(), null);
+		}
+
+		public static IEnumerable<T> GetControls<T>(this ControlCollection cCol, Func<T, bool> predicate) where T : WebControl
+		{
+			return cCol.GetControls(new List<T>(), predicate);
+		}
+	}
+}
